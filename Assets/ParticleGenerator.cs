16f04f2e722d43358758749a43c0637b280af680
Matt Cramblett <Cramblett.10@osu.edu>
2016-09-28 17:11:59 -0400

    drop falling physics implementation - note the new class called Drop

--- Assets/ParticleGenerator.cs
+++ Assets/ParticleGenerator.cs
@@ -9,14 +9,29 @@ public class ParticleGenerator : MonoBehaviour {
 	public float rate = -0.1f;
 	public int generateCount = 1;
 
+
+	public class Drop {
+		public GameObject body;
+		public float velocity;
+		public int life;
+
+		public Drop(){
+			body = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			velocity = 2f;
+			life = 0;
+		}
+	}
+
+
 	//creates sphere game objects:
 	void generateParticles(){
 		for(int i = 0; i < particleDensity; i++){
-     		GameObject aSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-     		aSphere.transform.parent = transform;
-     		aSphere.name = "sphere" + i.ToString();
-     		aSphere.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), yStartPosition, Random.Range(-4.0f, 4.0f)); //drop start pos
-			aSphere.transform.localScale = new Vector3(0.04f, 0.1f, 0.04f); //drop size
+     		//GameObject aSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+     		Drop aSphere = new Drop();
+     		aSphere.body.transform.parent = transform;
+     		aSphere.body.name = "sphere" + i.ToString();
+     		aSphere.body.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), yStartPosition, Random.Range(-4.0f, 4.0f)); //drop start pos
+			aSphere.body.transform.localScale = new Vector3(0.04f, 0.1f, 0.04f); //drop size
 			Particles.Add(aSphere);
 		}
 	}
@@ -29,28 +44,35 @@ public class ParticleGenerator : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		List<GameObject> toRemove = new List<GameObject> ();
-		foreach (GameObject p in Particles){
-			//add up forces acting on p
+		ArrayList toRemove = new ArrayList();
+		foreach (Drop p in Particles){
+			p.life++; //each frame is one point to life
 			GameObject ground = GameObject.FindGameObjectWithTag("ground");
 
-			if (p.transform.position.y > ground.transform.position.y+.02) {
-				p.transform.position += Vector3.up * 1 *rate; //cheaply make the rain fall
+			//if particle is above ground:
+			if (p.body.transform.position.y > ground.transform.position.y+.02) {
+				//drop falling physics:
+				float accel = 1f; //only force is gravity, using an equal ratio with mass
+				float endv = p.velocity + accel * Time.deltaTime; // v + a * deltaTime
+				p.body.transform.position -= Vector3.up * ((endv + p.velocity)/2) * Time.deltaTime; //update y position
+				p.velocity = endv; //update particle velocity
 			}
-			if (p.transform.position.y <= ground.transform.position.y + .02) {
-				Vector3 shape = p.transform.localScale;
+			//if particle is above ground:
+			if (p.body.transform.position.y <= ground.transform.position.y + .02) {
+				Vector3 shape = p.body.transform.localScale;
 				if (shape.y > 0) {
 					shape.y -= .01f;
 				} else {
 					toRemove.Add (p);
 				}
-				p.transform.localScale = shape;
+				p.body.transform.localScale = shape;
 			}
 		}
-		foreach (GameObject p in toRemove) {
-			//generateSmallParticles (p);
+		foreach (Drop p in toRemove) {
 			Particles.Remove (p);
 		}
+
+		//Key up and down to change density of drops:
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			if(particleDensity < 50){
 				particleDensity++;
